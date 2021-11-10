c311ddea51fa8e60d7ea77a76410fb28bf70726c
kazono <kazuki.o.h277@gmail.com>
2021-11-10 22:04:32 +0900

    Update Solution.java

--- src/main/java/jp/co/kazono/leetcode/LC0053_Maximum_Subarray/Solution.java
+++ src/main/java/jp/co/kazono/leetcode/LC0053_Maximum_Subarray/Solution.java
@@ -13,11 +13,8 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        //System.out.println(s.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
-        //System.out.println(s.maxSubArray(new int[]{1}));
+        System.out.println(s.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(s.maxSubArray(new int[]{1}));
         System.out.println(s.maxSubArray(new int[]{5, 4, -1, 7, 8}));
-
-        System.out.println(Math.max(9, 8));
-        System.out.println(Math.max(8, 9));
     }
 }
