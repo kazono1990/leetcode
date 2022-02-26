e6f4e2de733bbba6fb9b988bddfd8bb1e7dc2433
kazono <kazuki.o.h277@gmail.com>
2022-02-26 22:30:13 +0900

    add LC0847

---
+++ src/main/java/jp/co/kazono/leetcode/LC0847_Shortest_Path_Visiting_All_Nodes/Solution.java
@@ -0,0 +1,53 @@
+package jp.co.kazono.leetcode.LC0847_Shortest_Path_Visiting_All_Nodes;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+    public int shortestPathLength(int[][] graph) {
+        if (graph.length == 1) {
+            return 0;
+        }
+
+        int n = graph.length;
+        int endingMask = (1 << n) - 1;
+        boolean[][] seen = new boolean[n][endingMask];
+        ArrayList<int[]> queue = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            queue.add(new int[] {i, 1 << i});
+            seen[i][1 << i] = true;
+        }
+
+        int steps = 0;
+        while (!queue.isEmpty()) {
+            ArrayList<int[]> nextQueue = new ArrayList<>();
+            for (int i = 0; i < queue.size(); i++) {
+                int[] currentPair = queue.get(i);
+                int node = currentPair[0];
+                int mask = currentPair[1];
+                for (int neighbor : graph[node]) {
+                    int nextMask = mask | (1 << neighbor);
+                    if (nextMask == endingMask) {
+                        return 1 + steps;
+                    }
+
+                    if (!seen[neighbor][nextMask]) {
+                        seen[neighbor][nextMask] = true;
+                        nextQueue.add(new int[] {neighbor, nextMask});
+                    }
+                }
+            }
+            steps++;
+            queue = nextQueue;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        // 4
+        System.out.println(s.shortestPathLength(new int[][]{{1, 2, 3}, {0}, {0}, {0}}));
+    }
+}
